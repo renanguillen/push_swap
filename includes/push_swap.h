2128cdbd5bff161df536ca1747c3232d25970fe5
Renan Idalgo guillen carneiro <ridalgo-@c1r4p4.42sp.org.br>
2022-10-05 14:14:50 -0300

    Automatic commit via Makefile

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 15:46:25 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/10/04 16:32:39 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/10/04 17:38:57 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,25 @@
 # define PUSH_SWAP_H
 
 # include "../libft/includes/libft.h"
+
+// sa (swap a): Swap the first 2 elements at the top of stack a.
+// Do nothing if there is only one or no elements.
+// sb (swap b): Swap the first 2 elements at the top of stack b.
+// Do nothing if there is only one or no elements.
+// ss : sa and sb at the same time.
+// pa (push a): Take the first element at the top of b and put it at the top of a.
+// Do nothing if b is empty.
+// pb (push b): Take the first element at the top of a and put it at the top of b.
+// Do nothing if a is empty.
+// ra (rotate a): Shift up all elements of stack a by 1.
+// The first element becomes the last one.
+// rb (rotate b): Shift up all elements of stack b by 1.
+// The first element becomes the last one.
+// rr : ra and rb at the same time.
+// rra (reverse rotate a): Shift down all elements of stack a by 1.
+// The last element becomes the first one.
+// rrb (reverse rotate b): Shift down all elements of stack b by 1.
+// The last element becomes the first one.
+// rrr : rra and rrb at the same time
 
 #endif
--- sources/ft_push_swap.c
+++ sources/ft_push_swap.c
@@ -6,16 +6,17 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 15:45:05 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/10/04 15:57:02 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/10/04 18:02:32 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	int	i;
-
-	i = 0;
-	return (i);
+	if (argc < 3)
+		return (0);
+	else
+		return (ft_printf("%s", argv[argc - 1]));
+	return (0);
 }
