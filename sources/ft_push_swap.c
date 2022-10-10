7cb55c4d9afb621a581f794422ecb6a24f773d85
renanguillen <rigcpj@gmail.com>
2022-10-10 17:33:22 +0200

    Automatic commit via Makefile

--- sources/ft_push_swap.c
+++ sources/ft_push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 15:45:05 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/10/10 11:14:35 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/10/10 17:30:28 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 static void	entry_check (int argc, char **argv, t_element *a)
 {
+	// t_element	*new;
+
+	// new = ft_calloc(1, sizeof(t_element));
 	if (argc < 3)
 		exit (0);
 	else
@@ -29,12 +32,12 @@ static void	entry_check (int argc, char **argv, t_element *a)
 int	main(int argc, char **argv)
 {
 	t_element	*a;
-	t_element	*b;
+	// t_element	*b;
 
 	a = ft_calloc((argc - 1), sizeof(t_element));
-	b = ft_calloc((argc - 1), sizeof(t_element));
+	// b = ft_calloc((argc - 1), sizeof(t_element));
 	entry_check(argc, argv, a);
 	ft_pointerfree(a);
-	ft_pointerfree(b);
+	// ft_pointerfree(b);
 	return (0);
 }
