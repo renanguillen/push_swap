2c5c9b8bebf8ef19bea14eeaf2c11be38690080f
renanguillen <rigcpj@gmail.com>
2022-10-15 17:10:39 -0300

    Automatic commit via Makefile

--- sources/ft_push_swap.c
+++ sources/ft_push_swap.c
@@ -6,38 +6,26 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 20:49:55 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/10/13 21:29:46 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/10/15 17:00:27 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	ft_swap(t_element *stack)
-{
-	t_element	*x;
-	t_element	*y;
-
-	x->value = stack->value;
-	x->index = stack->index;
-	stack->value = stack->next->value;
-	stack->index = stack->next->index;
-	stack->next->value = x->value;
-	stack->next->index = x->index;
-}
 // void	ft_swap_three(t_element *stack)
 // {
 // 	if	(stack)
 // }
 
-void	ft_swap_two(t_element *stack)
+void	ft_swap_two(t_element **stack)
 {
-	if (stack->value < stack->next->value)
+	if ((*stack)->value < (*stack)->next->value)
 		exit(EXIT_SUCCESS);
 	else
 		ft_swap(stack);
 }
 
-void	ft_define_swap(int argc, t_element *stack)
+void	ft_define_swap(int argc, t_element **stack)
 {
 	if (argc == 3)
 		ft_swap_two(stack);
@@ -57,7 +45,7 @@ int	main(int argc, char **argv)
 	{
 		ft_get_stack(argc, argv, &stack);
 		ft_get_index(stack);
-		ft_define_swap(argc, stack);
+		ft_define_swap(argc, &stack);
 	}
 	while (stack)
 	{
--- sources/operations/ft_swap.c
+++ sources/operations/ft_swap.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/04 15:46:25 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/10/15 17:01:12 by ridalgo-         ###   ########.fr       */
+/*   Created: 2022/10/15 17:00:32 by ridalgo-          #+#    #+#             */
+/*   Updated: 2022/10/15 17:00:38 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "../../includes/push_swap.h"
 
-# include "../libft/includes/libft.h"
+void	ft_swap(t_element **stack)
+{
+	t_element	*aux;
 
-int		ft_error(void);
-void	ft_get_index(t_element *a);
-void	ft_get_stack(int argc, char**argv, t_element **a);
-int		ft_input_check(char *argv);
-
-void	ft_swap(t_element **stack);
-
-#endif
+	aux = (*stack)->next;
+	(*stack)->next = (*stack)->next->next;
+	aux->next = *stack;
+	*stack = aux;
+	(*stack)->position = 0;
+	(*stack)->next->position = 1;
+}
