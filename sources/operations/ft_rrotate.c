6488f143c8cd144e9b1a13d248994fff497390ce
renanguillen <rigcpj@gmail.com>
2022-10-19 17:40:34 -0300

    Automatic commit via Makefile

--- sources/operations/ft_rrotate.c
+++ sources/operations/ft_rrotate.c
@@ -6,7 +6,7 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 17:48:04 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/10/19 16:19:06 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/10/19 17:35:08 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,22 @@ void	ft_rrotate(t_element **stack, char c)
 {
 	t_element	*aux;
 	t_element	*top;
+	t_element	*bot;
 
 	aux = *stack;
-	while (aux)
+	while (aux->next->next)
+		aux = aux->next;
+	bot = aux;
+	aux = *stack;
+	while (aux->next)
 	{
 		aux->position++;
-		if (!(aux)->next)
-		{
-			top = aux;
-			top->next = (*stack);
-			(*stack) = top;
-			(*stack)->position = 0;
-		}
-		else
-			aux = aux->next;
+		aux = aux->next;
 	}
+	top = aux;
+	top->next = (*stack);
+	(*stack) = top;
+	(*stack)->position = 0;
+	bot->next = NULL;
 	ft_printf("rr%c\n", c);
 }
