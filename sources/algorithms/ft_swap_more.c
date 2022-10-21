54e49fbd5ce8879819754989737e5d0bd55db8c3
renanguillen <rigcpj@gmail.com>
2022-10-21 17:45:50 -0300

    Automatic commit via Makefile

--- sources/algorithms/ft_swap_more.c
+++ sources/algorithms/ft_swap_more.c
@@ -6,7 +6,7 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 16:34:08 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/10/21 16:37:36 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/10/21 17:41:07 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,30 +26,38 @@ static void	ft_print_stack(t_element **stack, char c)
 	}
 }
 
-
-
 void	ft_swap_more(t_element **stack_a)
 {
 	t_element	*stack_b;
 	t_element	*aux;
 	int			cut_index;
+	int			counter;
 
 	cut_index = 0;
+	counter = 3;
 	stack_b = NULL;
 	aux = *stack_a;
 	while (aux)
 	{
-		cut_index += aux->index;
+		cut_index++;
 		aux = aux->next;
 	}
 	cut_index /= 2;
 	aux = *stack_a;
 	while ((*stack_a)->next->next->next)
 	{
-		if ((*stack_a)->index < cut_index)
-			ft_push(stack_a, &stack_b, 'a');
+		if (counter)
+		{
+			if ((*stack_a)->index < cut_index)
+				ft_push(stack_a, &stack_b, 'a');
+			else
+			{
+				ft_rotate(stack_a, 'a');
+				counter--;
+			}
+		}
 		else
-			ft_rotate(stack_a, 'a');
+			ft_push(stack_a, &stack_b, 'a');
 	}
 	ft_swap_three(stack_a);
 	ft_get_target(stack_a, &stack_b);
