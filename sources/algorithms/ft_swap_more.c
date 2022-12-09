dfef9e666e92f90a7a868a8cd84634a02b348796
renanguillen <rigcpj@gmail.com>
2022-12-09 16:29:20 -0300

    Automatic commit via Makefile

--- sources/algorithms/ft_swap_more.c
+++ sources/algorithms/ft_swap_more.c
@@ -6,7 +6,7 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 16:34:08 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/12/09 16:27:18 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/12/09 16:29:06 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,7 @@ void	ft_swap_more(t_element **stack_a, t_element **stack_b, int stacklen)
 		new_lenght++;
 	}
 	ft_final_rotation(stack_a, stack_b, (new_lenght / 2));
+	stack_b = NULL;
 	// ft_print_stack(stack_a, 'A');
 	// ft_print_stack(stack_b, 'B');
 	return ;
